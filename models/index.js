49e5eed3e575b74120ba5f9be8b1308c53886c73
juanx198522 <juanx1985@icloud.com>
2023-08-24 21:51:57 -0400

    onDelete cascade added

--- models/index.js
+++ models/index.js
@@ -5,26 +5,32 @@ const Comment = require("./Comment");
 // Define associations between User, Post, and Comment models
 User.hasMany(Post, {
   foreignKey: "user_id",
+  onDelete: "cascade",
 });
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "cascade",
 });
 
 Comment.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "cascade",
 });
 
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
+  onDelete: "cascade",
 });
 
 Post.hasMany(Comment, {
   foreignKey: "post_id",
+  onDelete: "cascade",
 });
 
 User.hasMany(Comment, {
   foreignKey: "user_id",
+  onDelete: "cascade",
 });
 
 module.exports = { User, Post, Comment };
